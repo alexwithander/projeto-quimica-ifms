9795895b524c336f2fcdcdc03e7d5d711d9b9f43
Alexander <intelligencegeek@gmail.com>
2024-01-04 19:34:47 -0400

    exclusão de codigos comentados

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -7,25 +7,6 @@
         <span>Licenciatura em Quimica</span>
       </v-toolbar-title>
       </v-spacer>
-      <!-- <v-menu offset-y>
-        <template v-slot:activator="{ on }">
-          <v-btn text v-on="on">
-            <v-icon left ></v-icon>
-            <span>Menu</span>
-          </v-btn>
-        </template>
-        <v-list flat>
-          <v-list-item
-            v-for="link in links"
-            :key="link.text"
-            router
-            :to="link.route"
-            active-class="border"
-          >
-            <v-list-item-title>{{ link.text }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu> -->
     </v-app-bar>
     <v-navigation-drawer v-model="drawer" dark app color="#1B5E20">
       <v-layout column align-center>
