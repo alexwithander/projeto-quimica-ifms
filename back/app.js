b611ca6413ac033ba4bd4b9a1f3e830229fa1b21
Carlos <carlosxandresr@gmail.com>
2024-01-09 22:04:24 -0300

    conexão db + api

--- back/app.js
+++ back/app.js
@@ -1,37 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
+const db = require('./db/connection');
+
 const app = express();
 const PORT = 3000;
 const SECRET_KEY = 'SECRET_KEY';
 
 app.use(bodyParser.json());
 
-// Array de exemplo para simular eventos
-const eventos = [];
-
-// Simulação de um banco de dados
-const users = [
-  { id: 1, username: 'usuario1', password: 'senha123' },
-  { id: 2, username: 'usuario2', password: 'senha456' }
-];
-
 app.post('/api/login', (req, res) => {
   const { username, password } = req.body;
 
-  // Simulação de autenticação
-  const user = users.find(u => u.username === username && u.password === password);
+  db.query('SELECT * FROM usuarios WHERE username = ? AND password = ?', [username, password], (err, results) => {
+    if (err) {
+      console.error('Erro na consulta ao banco de dados:', err);
+      return res.status(500).json({ mensagem: 'Erro interno do servidor' });
+    }
+
+    const user = results[0];
+
 
   if (user) {
     // Gera um token
     const token = jwt.sign({ userId: user.id }, SECRET_KEY, { expiresIn: '2h' });
+    console.log('Token Gerado:', token);
 
     res.json({ mensagem: 'Login bem-sucedido', token });
   } else {
     res.status(401).json({ mensagem: 'Credenciais inválidas' });
   }
+  });
 });
 
+/* REMOVIDO TEMPORARIAMENTE (BUG)
 // Função para verificar o token
 function verificarToken(req, res, next) {
   const token = req.header('Authorization');
@@ -49,26 +51,41 @@ function verificarToken(req, res, next) {
     req.userId = decoded.userId;
     next();
   });
-}
-
-// Rota protegida com token do login para adicionar eventos
-app.post('/api/adicionarEvento', verificarToken, (req, res) => {
+} */
+app.post('/api/adicionarEvento', /* verificarToken, */ (req, res) => {
   const novoEvento = {
     evento: req.body.evento,
     organizador: req.body.organizador,
     data: req.body.data,
-    status: req.body.status,
   };
 
-  // Adiciona o novo evento ao array de eventos
-  eventos.push(novoEvento);
+  /// Adiciona o novo evento ao banco de dados
+  db.query('INSERT INTO eventos (evento, organizador, data) VALUES (?, ?, ?)', 
+    [novoEvento.evento, novoEvento.organizador, novoEvento.data], (err, result) => {
+      if (err) {
+        console.error('Erro ao adicionar evento:', err);
+        return res.status(500).json({ mensagem: 'Erro interno do servidor' });
+      }
 
-  res.json({ mensagem: 'Evento adicionado com sucesso', evento: novoEvento });
+      novoEvento.id = result.insertId;
+
+      res.json({ mensagem: 'Evento adicionado com sucesso', evento: novoEvento });
+  });
 });
 
 // Rota para listar todos os eventos
-app.get('/api/listarEventos', (req, res) => {
-  res.json({ eventos });
+app.get('/api/listarEventos', /* verificarToken, */ (req, res) => {
+  // Consulta a banco de dados para obter a lista de eventos
+  db.query('SELECT * FROM eventos', (err, results) => {
+    if (err) {
+      console.error('Erro na consulta ao banco de dados:', err);
+      return res.status(500).json({ mensagem: 'Erro interno do servidor' });
+    }
+
+    const eventos = results;
+
+    res.json({ eventos });
+  });
 });
 
 app.listen(PORT, () => {
